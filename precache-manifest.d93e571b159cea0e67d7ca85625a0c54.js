225ee776097cf52ad1218d40b0bb31e27abf8890
gabrielprrd <gabriel.afonso.contato@hotmail.com>
2020-09-02 13:37:01 +0100

    Updates

---
+++ precache-manifest.d93e571b159cea0e67d7ca85625a0c54.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e70041375e3d75607bab7174dd11f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bff65baba90aed7b2040",
+    "url": "/static/js/2.2717b4d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2717b4d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd9d663812b633e5294",
+    "url": "/static/js/main.0ed5af38.chunk.js"
+  },
+  {
+    "revision": "000167aa3d5fab317d3c",
+    "url": "/static/js/runtime-main.d11e5f44.js"
+  },
+  {
+    "revision": "83af227affe24fbe90bc8231ed2bdaca",
+    "url": "/static/media/DroidSansMono.83af227a.ttf"
+  },
+  {
+    "revision": "01238b4b8d74c33ee259b20091a21fdb",
+    "url": "/static/media/calorie-police.01238b4b.png"
+  },
+  {
+    "revision": "a3bafb7ebd836bd1069e1ccc8e59934c",
+    "url": "/static/media/curriculum-ide.a3bafb7e.png"
+  },
+  {
+    "revision": "b78e09bfd63acacbe0302a3c224b317a",
+    "url": "/static/media/folder-logo.b78e09bf.png"
+  },
+  {
+    "revision": "5fb9a7520a90094f5c6968169e817c97",
+    "url": "/static/media/gif-searcher.5fb9a752.png"
+  },
+  {
+    "revision": "e45ebd5258d77392cb78ceb7b3133eeb",
+    "url": "/static/media/github-logo.e45ebd52.png"
+  },
+  {
+    "revision": "e8c27f6b0fcf596a06a31135d937c73e",
+    "url": "/static/media/js-logo.e8c27f6b.png"
+  },
+  {
+    "revision": "b476fecaed90e278eb012cd1c1bdc8e0",
+    "url": "/static/media/linkedin-logo.b476feca.png"
+  },
+  {
+    "revision": "06a30a3661937881c95d6e56c1c512d6",
+    "url": "/static/media/nortada-ecommerce.06a30a36.png"
+  }
+]);
