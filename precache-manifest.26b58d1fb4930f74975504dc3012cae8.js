2320e4c4af48c5aaad1b1b494476033ec0595306
gabrielprrd <gabriel.afonso.contato@hotmail.com>
2020-09-02 13:23:19 +0100

    Updates

---
+++ precache-manifest.26b58d1fb4930f74975504dc3012cae8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bcf96624483ef98a1e53578fef3adbe",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "387da41f83180534a955",
+    "url": "/curriculum/static/js/2.0970c7f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/curriculum/static/js/2.0970c7f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f3f47c99b18e258539",
+    "url": "/curriculum/static/js/main.4fbc7117.chunk.js"
+  },
+  {
+    "revision": "d26c9f8e437b7977ea03",
+    "url": "/curriculum/static/js/runtime-main.65dcdb1b.js"
+  },
+  {
+    "revision": "83af227affe24fbe90bc8231ed2bdaca",
+    "url": "/curriculum/static/media/DroidSansMono.83af227a.ttf"
+  },
+  {
+    "revision": "01238b4b8d74c33ee259b20091a21fdb",
+    "url": "/curriculum/static/media/calorie-police.01238b4b.png"
+  },
+  {
+    "revision": "a3bafb7ebd836bd1069e1ccc8e59934c",
+    "url": "/curriculum/static/media/curriculum-ide.a3bafb7e.png"
+  },
+  {
+    "revision": "b78e09bfd63acacbe0302a3c224b317a",
+    "url": "/curriculum/static/media/folder-logo.b78e09bf.png"
+  },
+  {
+    "revision": "5fb9a7520a90094f5c6968169e817c97",
+    "url": "/curriculum/static/media/gif-searcher.5fb9a752.png"
+  },
+  {
+    "revision": "e45ebd5258d77392cb78ceb7b3133eeb",
+    "url": "/curriculum/static/media/github-logo.e45ebd52.png"
+  },
+  {
+    "revision": "e8c27f6b0fcf596a06a31135d937c73e",
+    "url": "/curriculum/static/media/js-logo.e8c27f6b.png"
+  },
+  {
+    "revision": "b476fecaed90e278eb012cd1c1bdc8e0",
+    "url": "/curriculum/static/media/linkedin-logo.b476feca.png"
+  },
+  {
+    "revision": "06a30a3661937881c95d6e56c1c512d6",
+    "url": "/curriculum/static/media/nortada-ecommerce.06a30a36.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d93e571b159cea0e67d7ca85625a0c54.js"
+  "/curriculum/precache-manifest.26b58d1fb4930f74975504dc3012cae8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/curriculum/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
